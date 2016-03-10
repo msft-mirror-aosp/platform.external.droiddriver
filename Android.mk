3d5506feee049cc6f5cb8975053e991f6702a365
Neil Fuller <nfuller@google.com>
2016-03-10 13:10:29 +0000

    Return a target to using 1.8
    It is currently unclear why this is needed.
    
    Bug: 27584859
    Change-Id: I54423e64e7d84926a44e57365e5c4d5a0ae74cb3

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_MODULE := droiddriver
 LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := 19
+# Workaround for http://b/27584859
+LOCAL_JAVA_LANGUAGE_VERSION := 1.8
 
 LOCAL_JAVACFLAGS += -Xlint:deprecation -Xlint:unchecked
 
